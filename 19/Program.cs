2b1166fe98183e776ca7b77441d200ec87fb0afa
Arthur Patrakhaev <redred05@mail.ru>
2022-09-09 01:09:08 +0300

    initial commit

---
+++ 19/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+int x;
+int sum = 0;
+int numX;
+Console.WriteLine("Введите пятизначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+for (numX = num; num != 0; num = num / 10)
+{
+    x = num % 10;
+    sum = sum * 10 + x;
+}
+if (numX == sum)
+{
+    Console.Write($"{numX} Это палиндром");
+}
+else
+{
+    Console.Write($"{numX} Не палиндром");
+}
---
+++ 23/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Таблица кубов чисел");
+for (int i = 1; i <= num; i++)
+{
+    Console.Write($"{i * i * i} ");
+}
